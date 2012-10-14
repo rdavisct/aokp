3ab7174cd8c570a9b5a8bd1b415c41990a4e8d9e
rdavisct <rdavisct@gmail.com>
2012-10-14 13:58:29 -0400

    first commit

---
+++ common/common_phone.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+PRODUCT_PACKAGES += \
+    NovaLauncher
+
+# Inherit drm blobs
+-include vendor/aokp/configs/common_drm_phone.mk
+
+# BT config
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
